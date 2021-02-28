ba6b28798e380619741008694f98477622b66ace
DonPerinon <rastislav.pobis@gmail.com>
2021-02-28 12:12:41 +0100

    Beta

--- APP1/dist/app.js
+++ APP1/dist/app.js
@@ -1,6 +1,3 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.APP = void 0;
-class APP {
-}
-exports.APP = APP;
+console.log('saaaa');
+//# sourceMappingURL=app.js.map
--- APP1/dist/index.js
+++ APP1/dist/index.js
@@ -4,9 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const maradb = require('mariadb');
+const path_1 = __importDefault(require("path"));
 const app = express_1.default();
-app.listen(3000, () => console.log('Ahoj'));
+app.listen(3000, () => console.log('Express server running at http://127.0.0.1:3000'));
 app.get('/', function (request, response) {
-    response.send('Hello World!');
+    response.sendFile(path_1.default.join(__dirname, '..', 'index.html'));
 });
+app.use(express_1.default.static(__dirname));
+//# sourceMappingURL=index.js.map
