95ad88f3951f5d503ba7f9e3eaeceb6a1a4651a5
DonPerinon <rastislav.pobis@gmail.com>
2021-02-28 12:16:08 +0100

    Beta

--- APP1/src/app.ts
+++ APP1/src/app.ts
@@ -1,4 +1,1 @@
-export class APP{
-
-    
-}
+console.log('saaaa');
--- APP1/src/index.ts
+++ APP1/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-const maradb= require('mariadb');
-
+import mariadb from 'mariadb';
+import path from "path";
 const app= express();
-app.listen(3000,()=>console.log('Ahoj'));
+app.listen(3000,()=>console.log('Express server running at http://127.0.0.1:3000'));
 app.get('/', function(request, response) {
-    response.send('Hello World!')
+    response.sendFile(path.join(__dirname,'..','index.html'))
   })
+app.use(express.static(__dirname));
+
