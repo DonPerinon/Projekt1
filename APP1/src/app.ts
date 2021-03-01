63428f03902b84675c09ca94bca39a359706ebed
DonPerinon <rastislav.pobis@gmail.com>
2021-03-01 09:12:41 +0100

    Beta 1.0.1

--- APP1/src/app.ts
+++ APP1/src/app.ts
@@ -1,1 +1,19 @@
-console.log('saaaa');
+function GetAnimal():void{
+ const Animal = GetInputValue("Animal");
+console.log(Animal);
+
+
+
+}
+function GetInputValue(elementID:string):string | undefined{
+const inputElement:HTMLInputElement = <HTMLInputElement>document.getElementById(elementID)
+
+if(inputElement.value==='')
+{
+    return undefined;
+}
+else{
+    return inputElement.value;
+}
+}
+document.getElementById('showanimal')?.addEventListener('click',GetAnimal);
