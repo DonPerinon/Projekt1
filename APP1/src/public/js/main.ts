996c844691c9c7c80b97744bdc18e7eeed978f3d
DonPerinon <rastislav.pobis@gmail.com>
2021-03-03 23:04:15 +0100

    working app

---
+++ APP1/src/public/js/main.ts
@@ -0,0 +1,73 @@
+import axios from "axios";
+import * as M from "materialize-css";
+import Vue from "vue";
+
+// tslint:disable-next-line no-unused-expression
+new Vue( {
+   
+    computed: {
+    hazGuitars(): boolean {
+        console.log(this.animals);
+        console.log(this.isLoading);
+            return this.isLoading === false && this.animals.length > 0;
+        },
+     noGuitars(): boolean {
+            console.log(this.animals);
+            console.log(this.isLoading);
+            return this.isLoading === false && this.animals.length === 0;
+        
+        }
+    },
+    data() {
+        return {
+            Name:"",
+            Image:Image,
+            animals:[],
+            isLoading: true
+            //text:""
+        };
+    },
+    el: "#app",
+    methods: {
+       /* addAnimal() {
+            const animal = {
+              name: this.name,
+              //text :this.text
+            };
+            axios
+                .post( "/api/animal/add",animal )
+                .then( () => {
+                  
+                    this.name="";
+                    this.text= "";
+                    this.loadAnimals();
+                } )
+                .catch( ( err: any ) => {
+                   
+                    console.log( err );
+                } );
+        },*/
+        
+        loadAnimals() {
+            axios
+                .get( "/api/animal/all" )
+                .then( ( res: any ) => {
+                    
+                    this.isLoading = false;
+                    console.log(res.data)
+                    this.animals = res.data;
+
+                   
+                } )
+                .catch( ( err: any ) => {
+             
+                    console.log( err );
+                } );
+        }
+    },
+    mounted() {
+        console.log(this.animals)
+        return this.loadAnimals();
+     
+    }
+} );
---
+++ APP1/src/routes/index.ts
@@ -0,0 +1,17 @@
+import * as express from "express";
+import * as api from "./api";
+
+export const register = ( app: express.Application ) => {
+    const oidc = app.locals.oidc;
+
+    // define a route handler for the default home page
+    app.get( "/", ( req: any, res ) => {
+        res.render( "index" );
+    } );
+    app.get( "/animal", ( req: any, res ) => {
+        res.render( "animals" );
+    } );
+
+
+    api.register( app );
+};
