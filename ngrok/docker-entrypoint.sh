232dddd160a07dd6cd7e7ec9d0bba6cffa127f38
DonPerinon <rastislav.pobis@gmail.com>
2021-03-09 09:17:22 +0100

    Etrypoint parser Final

--- ngrok/docker-entrypoint.sh
+++ ngrok/docker-entrypoint.sh
@@ -3,5 +3,5 @@
 echo "entrypoint $APP"
 /usr/local/bin/ngrok http --config=/opt/ngrok/conf/ngrok.yml $NGROK_APP &
 sleep 5
-curl localhost:4040/api/tunnels | jq -r '.tunnels[] | .public_url'| egrep 'https:' >> /tmp/endpoints
+curl localhost:4040/api/tunnels | jq -r '.tunnels[] | .public_url'|egrep 'https:' |awk '{print "- "$1}' >> /tmp/endpoints
 exec "$@"
