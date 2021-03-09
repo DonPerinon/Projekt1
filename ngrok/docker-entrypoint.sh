329eb1fbf43414c8a30625e398ebd987fe47d297
DonPerinon <rastislav.pobis@gmail.com>
2021-03-09 08:09:02 +0100

    Compose

---
+++ ngrok/docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "entrypoint $APP"
+/usr/local/bin/ngrok http --config=/opt/ngrok/conf/ngrok.yml $NGROK_APP &
+sleep 5
+curl localhost:4040/api/tunnels | jq -r '.tunnels[] | .public_url' >> /tmp/endpoints
+exec "$@"
