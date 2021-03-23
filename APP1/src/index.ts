57f225b9a93b926d4c392a5e2c88e67784d58596
DonPerinon <rastislav.pobis@gmail.com>
2021-03-23 11:17:06 +0100

    KeyCloak conf

--- APP1/src/index.ts
+++ APP1/src/index.ts
@@ -1,33 +1,34 @@
 
-import { createRequire } from 'module';
 import * as shell from "shelljs";
 import * as routes from "./routes";
-shell.cp( "-R", "src/views", "dist/" );
+shell.cp("-R", "src/views", "dist/");
 // const require = createRequire(import.meta.url);
 import path from 'path'
 // import { fileURLToPath, pathToFileURL } from 'node:url';
 // const __dirname = path.dirname(__filename);
 // import * as mariadb from '../node_modules/mariadb/package.json';
-const {initKeycloak,getKeycloak} = require('./keycloak')
-import { request } from 'http';
 
+import { request } from 'http';
+const {initKeycloak,
+getKeycloak} = require('../tools/keycloak.ts')
 // import mariadb from'mariadb';
 import mariadb from 'mariadb';
 // import express from 'express';
 // import express from '../node_modules/@types/express/';
 import express from 'express';
-const app= express();
+const app = express();
 
 
-app.listen(3000,()=>{console.log('Express server running at http://127.0.0.1:3000')});
-app.set( "views", path.join( __dirname, "views" ) );
-app.set( "view engine", "ejs" );
-app.use( express.json() );
-app.get('/', function(request, response) {
-   response.render( "index" );
-  })
-  // Routes registerd
+initKeycloak()
+app.listen(3000, () => { console.log('Express server running at http://127.0.0.1:3000') });
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.json());
+app.get('/', function (request, response) {
+  response.render("index");
+})
+// Routes registerd
 routes.register(app);
-  app.use( express.static( path.join( __dirname, "public" ) ) );
+app.use(express.static(path.join(__dirname, "public")));
 
 
--- APP1/tools/keycloak.ts
+++ APP1/tools/keycloak.ts
@@ -1,9 +1,9 @@
-var session = require('express-session');
-var Keycloak = require('keycloak-connect');
+let session = require('express-session');
+let Keycloak = require('keycloak-connect');
 
 let _keycloak;
 
-var keycloakConfig = {
+let keycloakConfig = {
     clientId: 'animalapp',
     bearerOnly: true,
     serverUrl: 'http://keycloak:8080/auth',
@@ -17,10 +17,10 @@ function initKeycloak() {
     if (_keycloak) {
         console.warn("Trying to init Keycloak again!");
         return _keycloak;
-    } 
+    }
     else {
         console.log("Initializing Keycloak...");
-        var memoryStore = new session.MemoryStore();
+        const memoryStore = new session.MemoryStore();
         _keycloak = new Keycloak({ store: memoryStore }, keycloakConfig);
         return _keycloak;
     }
@@ -29,7 +29,7 @@ function initKeycloak() {
 function getKeycloak() {
     if (!_keycloak){
         console.error('Keycloak has not been initialized. Please called init first.');
-    } 
+    }
     return _keycloak;
 }
 
