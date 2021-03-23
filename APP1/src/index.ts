6e8ab00667fcdb8da3ad34d78e2a7ec5747794e7
root <root@NB-POBIS.localdomain>
2021-03-23 09:10:04 +0100

    KeyCloak

--- APP1/src/index.ts
+++ APP1/src/index.ts
@@ -8,7 +8,7 @@ import path from 'path'
 // import { fileURLToPath, pathToFileURL } from 'node:url';
 // const __dirname = path.dirname(__filename);
 // import * as mariadb from '../node_modules/mariadb/package.json';
-
+const {initKeycloak,getKeycloak} = require('./keycloak')
 import { request } from 'http';
 
 // import mariadb from'mariadb';
@@ -18,27 +18,6 @@ import mariadb from 'mariadb';
 import express from 'express';
 const app= express();
 
-
-export const db = mariadb.createPool({
-   host:'localhost',
-   port:3385,
-   password:'mypass',
-   user:'root',
-   database:'app_db',
-   connectionLimit: 5
- });
-
-export async function asyncFunction() {
-   let conn;
-   try {
-     conn = await db.getConnection();
-     const rows = await conn.query("SELECT Name from Animal;");
-     console.log(rows); // [ {val: 1}, meta: ...
-   } catch (err) {
-     throw err;
-   } finally {
-     if (conn) return conn.end();
-   } };
 
 app.listen(3000,()=>{console.log('Express server running at http://127.0.0.1:3000')});
 app.set( "views", path.join( __dirname, "views" ) );
